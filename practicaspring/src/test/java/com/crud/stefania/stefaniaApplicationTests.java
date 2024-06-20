81f3b9089d123d70913b906e19993c7a8302dcfd
Pandora0623 <168572355+Pandora0623@users.noreply.github.com>
2024-06-19 22:53:31 -0500

    Stefania Delgado Cardona

---
+++ practicaspring/src/test/java/com/crud/stefania/stefaniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.stefania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class stefaniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
